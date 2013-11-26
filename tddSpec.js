6af65756fb604c082ce036345f3960aff4bcbdf7
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-26 10:50:11 +0100

    RED: converts lowercase string to uppercase

--- tddSpec.js
+++ tddSpec.js
@@ -1,15 +1,10 @@
-
-
-
-
-
-
 chai.should(); // invoking this function creates a "should" object on every object
 context = describe;
 
 describe("String parser", function(){
-    it("", function(){
-
+    it("converts lowercase string to uppercase", function(){
+        var result = parseString("desarrollador");
+        expect(['DESARROLADOR']).toEqual(result);
     });
 });
 
