2e2ee0a1fc44aa80941631ab0e8e73943e3494b2
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-26 12:57:19 +0100

    RED: removes articles

--- tddSpec.js
+++ tddSpec.js
@@ -1,63 +1,92 @@
 chai.should(); // invoking this function creates a "should" object on every object
 context = describe;
 
-function parseString(string){
-    var result = string.toUpperCase();
-    result = singularize(result);
-    result = replaceTildes(result);
-    return [result];
-}
+var parser = function() {
 
-function singularize (word) {
-    if (isPluralWord(word)) {
-        word = word.slice(0, -1);
+     cases = {
+            "Á": "A",
+            "É": "E",
+            "Í": "I",
+            "Ó": "O",
+            "Ú": "U"
+        };
+
+    var replaceTildes = function (word){
+        var result = word;
+        for (var letter in cases){
+            result = result.replace(letter, cases[letter]);
+        }
+
+        return result;
     }
 
-    return word;
-}
+    var singularize = function(word) {
+        if (isPluralWord(word)) {
+            word = word.slice(0, -1);
+        }
 
-function isPluralWord(word) {
-    return word.charAt(word.length -1) == 'S';
-}
-
-function replaceTildes(word){
-    var result = word;
-    var cases = {"Á":"A", "Ó":"O", "Í":"I"};
-    for (var letter in cases){
-        result = result.replace(letter, cases[letter]);
+        return word;
     }
-    return result;
-}
+
+    var isPluralWord = function(word) {
+        return word.charAt(word.length -1) == 'S';
+    }
+
+    var parseString = function(string){
+        var result = string.toUpperCase();
+        result = singularize(result);
+        result = replaceTildes(result);
+        return [result];
+    }
+
+
+    return {
+        parseString: parseString
+    };
+
+}();
+
 
 describe("String parser", function(){
+
+    beforeEach(function(){
+    var testParser = parser;
+        
+    });
+
     it("converts lowercase string to uppercase", function(){
-        var result = parseString("desarrollador");
+        var result = parser.parseString("desarrollador");
         expect(['DESARROLLADOR']).toEqual(result);
     });
 
     it("converts lowercase string to uppercase triangulating", function(){
-        var result = parseString('informatico');
+        var result = parser.parseString('informatico');
         expect(['INFORMATICO']).toEqual(result);
     });
 
     it("slices the last 's' character from a word", function(){
-        var result = parseString('INFORMATICOS');
+        var result = parser.parseString('INFORMATICOS');
         expect(['INFORMATICO']).toEqual(result);
     });
 
     it("replaces any 'tilde' character", function(){
-        var result = parseString('INFORMÁTICO');
+        var result = parser.parseString('INFORMÁTICO');
         expect(['INFORMATICO']).toEqual(result);
     });
 
     it("replaces any 'tilde' character triangulating" ,function(){
-        var result = parseString('CÓDIGO');
+        var result = parser.parseString('CÓDIGO');
         expect(['CODIGO']).toEqual(result);
     });
 
     it("replaces any 'tilde' character triangulating" ,function(){
-        var result = parseString('BARDAJÍ');
+        var result = parser.parseString('BARDAJÍ');
         expect(['BARDAJI']).toEqual(result);
+    });
+
+    it("removes articles", function(){
+        var result = parser.parseString('EL INFORMATICO');
+        expect(['INFORMATICO']),toEqual(result);
     });
 
 });
