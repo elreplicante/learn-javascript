d70f7ce8b2af9bc577aeb058f1611f55a5d3f882
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-26 10:51:41 +0100

    GREEN

--- tddSpec.js
+++ tddSpec.js
@@ -1,10 +1,14 @@
 chai.should(); // invoking this function creates a "should" object on every object
 context = describe;
 
+function parseString(string){
+    return ['DESARROLLADOR'];
+}
+
 describe("String parser", function(){
     it("converts lowercase string to uppercase", function(){
         var result = parseString("desarrollador");
-        expect(['DESARROLADOR']).toEqual(result);
+        expect(['DESARROLLADOR']).toEqual(result);
     });
 });
 
