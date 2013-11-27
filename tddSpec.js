93b93f84856eee2c7a7a9744eb0afd3298f2a6bb
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-27 11:34:41 +0100

    Moving parser to a file

--- tddSpec.js
+++ tddSpec.js
@@ -1,67 +1,6 @@
 chai.should(); // invoking this function creates a "should" object on every object
 context = describe;
 
-var parser = function() {
-
-    cases = {
-        "Á": "A",
-        "É": "E",
-        "Í": "I",
-        "Ó": "O",
-        "Ú": "U"
-        };
-
-    articles = {
-        "EL": "",
-        "LA": "",
-        "LO": ""
-    };
-
-    var sanitize = function(sentence, set) {
-        var result = sentence;
-        for(var item in set) {
-            result = result.replace(item, set[item]);
-        }
-
-        return result;
-    }
-
-    var singularize = function(word) {
-        if (isPluralWord(word)) {
-            word = word.slice(0, -1);
-        }
-
-        return word;
-    }
-
-    var isPluralWord = function(word) {
-        return word.charAt(word.length -1) == 'S';
-    }
-
-    var truncateArticles = function(element, index, array){
-            for (var item in articles) {
-                if (element == item) {
-                    array.splice(index, 1);
-                }
-            }
-        }
-
-    var parseString = function(string){
-        var result = string.toUpperCase();
-        result = result.split(" ");
-        result.forEach(truncateArticles);
-        for (var i = 0; i < result.length; i++) {
-            result[i] = sanitize(result[i], cases);
-            result[i] = singularize(result[i]);
-        };
-
-        return result;
-    }
-
-    return { parseString: parseString   };
-
-}();
-
 describe("String parser", function(){
 
     function parse(input) {
